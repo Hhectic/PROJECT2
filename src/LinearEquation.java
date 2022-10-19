005d2f3bbc69506f236c690b86f3e499869c951f
dara2077 <dbenyacar1@gmail.com>
2022-10-19 12:34:53 -0400

    da

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -16,8 +16,53 @@ public class LinearEquation {
     public double distance(){
         double x = x2 - x1;
         double y = y2 - y1;
-        double dis = Math.pow((Math.pow(x,2)))
+        double dis = Math.round(Math.pow((Math.pow(x,2)+Math.pow(y,2)),0.5)*100)/100;
+        return dis;
     }
+
+    public double yIntercept(){
+        double yint = y2 - slope()*x2;
+        return yint;
+    }
+
+    public double slope(){
+        double slop = Math.round(((double)(y2 - y1)/(x2-x1))*100.00);
+        double HA = slop/100;
+        return HA;
+    }
+
+    public String equation(){
+        int x = x2-x1;
+        int y = y2-y1;
+
+        if(x<0){
+            x = Math.abs(x);
+            y = -y;
+        }
+        if
+        String slope = y + "/" + x;
+
+        if(yIntercept()<0){
+            return "y = " + slope + "x" + " - " + Math.abs(yIntercept());
+        } else {
+            return "y = " + slope + "x" + " + " + Math.abs(yIntercept());
+        }
+    }
+    public double roundedToHundredth(double toRound){
+        double hund = Math.round((toRound * 100.00))/100.00;
+        return hund;
+    }
+
+    public String lineInfo(){
+        return "The two points are : " + "(" + x1 + "," + x2 + ")" + " and (" + x2 + "," + y2 + ")" + "\n" + "The equation of the line between these two points is: " + equation() + "\n" + "The slope of"
+    }
+
+
+
+
+
+
+
 
 
 
--- src/Man.java
+++ src/Man.java
@@ -7,9 +7,14 @@ public class Man {
         System.out.println("Enter coordinate two: ");
         String ok2 = omg.nextLine();
 
-        int sai = Integer.parseInt(ok);
-        int sai2 = Integer.parseInt(ok2);
+        int xone = Integer.parseInt(ok.substring(1,ok.indexOf(",")));
+        int xtwo = Integer.parseInt(ok2.substring(1,ok2.indexOf(",")));
+        int yone = Integer.parseInt(ok.substring(ok.indexOf(",")+1,ok.indexOf(")")));
+        int ytwo = Integer.parseInt(ok2.substring(ok2.indexOf(",")+1,ok2.indexOf(")")));
 
+        LinearEquation YO = new LinearEquation(xone, yone, xtwo, ytwo);
+        String equa = YO.equation();
+        System.out.print(xone + " " +  xtwo + " " + yone + " " + ytwo + " " + YO.slope() + " " + YO.equation() + " " + YO.yIntercept()) ;
 
 
 
